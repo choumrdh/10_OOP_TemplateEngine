8f293fa8c60686b762d9b4d8f5c76fc70d778e5a
Michelle Chou <chou.m.rdh@gmail.com>
2020-05-03 16:23:43 -0700

    finished questions

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
+
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -10,6 +11,162 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const managerQuestions =[
+     {
+        type: "input",
+        name: "Name",
+        message: "What is your manager's name?",
+        validate: function validateName(name) {
+            if (name === "") {
+                console.log("Please enter name");
+                return false
+            } else {
+                return true
+            }
+        }
+    }, {
+        type: "input",
+        name: "Id",
+        message: "What is your manager's ID?",
+        validate: function validateId(id) {
+            if (parseInt(id)) {
+                return true
+            } else {
+                console.log("Please enter a number for ID")
+                return false;
+            }
+        }
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is your manager's email?",
+        validate: function validateEmail(email) {
+            if (email.includes("@") && email.includes(".")) {
+                console.log(". Thank you for provide an valid email")
+                return true;
+            } else {
+                console.log(". Please Enter a valid email")
+                return false;
+            }
+        }
+    }, {
+        type: "input",
+        name: "officeNumber",
+        message: "What is your manager's office number?",
+        validate: function validateId(id) {
+            if (parseInt(id)) {
+                return true
+            } else {
+                console.log("Please enter a number for ID")
+                return false;
+            }
+        }
+    }]
+const addMemberQuestion=[
+    {
+        type:"list",
+        name:"roleOfMemeber",
+        message:"Which type of team member would you like to add?",
+        choices:["Engineer", "Intern", "No More Team"]
+    }
+];
+const engineerQuestions=[
+    {
+        type: "input",
+        name: "Name",
+        message: "What is the engineer's name?",
+        validate: function validateName(name) {
+            if (name === "") {
+                console.log("Please enter name");
+                return false
+            } else {
+                return true
+            }
+        }
+    }, {
+        type: "input",
+        name: "Id",
+        message: "What is the engineer's ID?",
+        validate: function validateId(id) {
+            if (parseInt(id)) {
+                return true
+            } else {
+                console.log("Please enter a number for ID")
+                return false;
+            }
+        }
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+        validate: function validateEmail(email) {
+            if (email.includes("@") && email.includes(".")) {
+                console.log(". Thank you for provide an valid email")
+                return true;
+            } else {
+                console.log(". Please Enter a valid email")
+                return false;
+            }
+        }
+    }, {
+        type: "input",
+        name: "engineerGithub",
+        message: "What is the engineer's github username?",
+    }
+]
+const internQuestions=[
+    {
+        type: "input",
+        name: "Name",
+        message: "What is the intern's name?",
+        validate: function validateName(name) {
+            if (name === "") {
+                console.log("Please enter name");
+                return false
+            } else {
+                return true
+            }
+        }
+    }, {
+        type: "input",
+        name: "Id",
+        message: "What is the intern's ID?",
+        validate: function validateId(id) {
+            if (parseInt(id)) {
+                return true
+            } else {
+                console.log("Please enter a number for ID")
+                return false;
+            }
+        }
+    }, {
+        type: "input",
+        name: "email",
+        message: "What is the 's email?",
+        validate: function validateEmail(email) {
+            if (email.includes("@") && email.includes(".")) {
+                console.log(". Thank you for provide an valid email")
+                return true;
+            } else {
+                console.log(". Please Enter a valid email")
+                return false;
+            }
+        }
+    }, {
+        type: "input",
+        name: "schoolName",
+        message: "What is the intern's school?",
+    }
+]
+inquirer.prompt(managerQuestions)
+.then(res => {
+    console.log(res)
+}).catch(err => {
+    console.log(err)
+})
+// function validateEmail(email) {
+//     return Joi.validate(email, Joi.string().email(), onValidation);
+// const questions=[]
 
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
