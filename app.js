347eb8cea7ef5ac187ac595d9b6f1447d5249cb8
Michelle Chou <chou.m.rdh@gmail.com>
2020-05-04 21:44:31 -0700

    clear up code

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const Intern = require("./lib/Intern");
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
-const util = require("util")
+
 const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
@@ -212,6 +212,3 @@ async function init() {
 }
 
 init();
-
-
-
